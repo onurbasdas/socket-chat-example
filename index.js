d69cda701c2861e1be96c796a37cf389316564eb
onurbasdas <onurebasdase@hotmail.com>
2021-01-17 21:44:59 +0300

    port changes

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
+const PORT = 3000 || process.env.PORT;
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
+  console.log("a user connected");
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
     io.emit("chat message", msg);
   });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
 });
